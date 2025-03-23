1e01cca80516a4ffea87e2c224e316abc1f29f9b
GARAPATI HARSHITA <garapatiharshita123@gmail.com>
2025-03-23 16:43:44 +0530

    Add files via upload

---
+++ Stack.py
@@ -0,0 +1,10 @@
+#To write a program to execute stack in the datastructures
+list=[] #To execute the stack it has to be empty
+list.append(2) 
+list.append(4)
+list.append(3)   #Here the append function used to push the element in the stack
+print(list) #print the stack 
+
+#Now the pop operation can be perform by pop()
+list.pop(2) #Here the element can be remove from the stack by pop()
+print(list)
